2b6580db95780e013c62fd17f0b833800c870010
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-20 23:44:42 +0800

    Create MyApplication.java

---
+++ app/src/main/java/com/tufanakcay/androidwebview/MyApplication.java
@@ -0,0 +1,19 @@
+package com.yourpackage.yourappname; 
+// Sesuaikan dengan package Anda
+
+import android.app.Application;
+import com.google.android.gms.ads.MobileAds;
+
+public class MyApplication extends Application {
+    private AppOpenManager appOpenManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        MobileAds.initialize(this, initializationStatus -> {
+            // Inisialisasi AppOpenManager setelah AdMob diinisialisasi
+            appOpenManager = new AppOpenManager(this);
+        });
+    }
+}
